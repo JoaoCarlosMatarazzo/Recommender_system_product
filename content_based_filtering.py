d1bae3fb2e6ac00dc30add3dc3b9a99d1a4ebbb8
João Carlos Matarazzo <100213241+JoaoCarlosMatarazzo@users.noreply.github.com>
2024-07-22 18:01:02 -0300

    Delete content_based_filtering.py

--- content_based_filtering.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics.pairwise import linear_kernel
-
-def get_content_recommendations(product_id, cosine_sim, products, n=10):
-    idx = products[products['product_id'] == product_id].index[0]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:n+1]
-    product_indices = [i[0] for i in sim_scores]
-    return products.iloc[product_indices]
